6999e1b0da193a9944eff9e365a94d6885a7e3f0
Bruno Vetorazo <vetorazo@gmail.com>
2025-07-29 22:15:02 +0900

    Got tom-select to work now trying to fix routes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :lens
   has_many :reviews, dependent: :destroy
+  has_many_attached :photos
 
   validates :owner_description, presence: true, length: { minimum: 10 }
   validates :daily_rate, presence: true, numericality: { greater_than: 0 }
